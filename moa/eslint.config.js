bbe5c6b1e56202b2792858d7eda70bcf3623c4ae
최이서 <chtoqur@gmail.com>
2025-10-13 12:05:26 +0900

    [SGK-72] chore: eslint 설정

--- moa/eslint.config.js
+++ moa/eslint.config.js
@@ -1,8 +1,12 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import configPrettier from 'eslint-config-prettier'
+import importPlugin from 'eslint-plugin-import'
+import prettier from 'eslint-plugin-prettier'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
 
 export default defineConfig([
   globalIgnores(['dist', 'node_modules']),
@@ -13,11 +17,14 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
-      // configPrettier
+      configPrettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -25,12 +32,70 @@ export default defineConfig([
       },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      // prettier: prettier,
+      react,
+      prettier,
+      import: importPlugin,
     },
     rules: {
+      // React
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'react/jsx-boolean-value': ['error', 'always'],
+      'react/jsx-no-useless-fragment': 'warn',
+      'react/jsx-no-bind': ['warn', { allowArrowFunctions: true }],
+
+      // React Hooks
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      // Import
+      'import/order': [
+        'error',
+        {
+          pathGroups: [
+            { pattern: 'react*', group: 'builtin', position: 'before' },
+            { pattern: '@src/**', group: 'external', position: 'after' },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          alphabetize: { order: 'asc' },
+        },
+      ],
+      'import/namespace': 'off',
+      'import/no-named-as-default': 'off',
+
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-debugger': 'error',
+      'no-duplicate-imports': 'error',
+      'prefer-const': 'error',
+      eqeqeq: ['error', 'always'],
+
+      // Prettier
+      'prettier/prettier': [
+        'error',
+        {
+          trailingComma: 'all',
+          tabWidth: 2,
+          semi: false,
+          singleQuote: true,
+          printWidth: 100,
+          arrowParens: 'always',
+          bracketSpacing: true,
+          bracketSameLine: false,
+          endOfLine: 'auto',
+        },
+      ],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        alias: {
+          map: [['@', './src']],
+          extensions: ['.js', '.jsx', '.json'],
+        },
+      },
     },
   },
 ])
