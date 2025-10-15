ee46678d4297d615913b4f3a13aad651d8183a3e
최이서 <chtoqur@gmail.com>
2025-10-15 17:51:36 +0900

    [SGK-72] chore: lint-staged 경로 변경

--- moa/src/icons/ReactIcon.jsx
+++ moa/src/icons/ReactIcon.jsx
@@ -2,7 +2,6 @@ import PropTypes from './types'
 import ReactSvg from '@/assets/react.svg?react'
 
 const ReactIcon = ({ width, height, fill, stroke, className }) => {
-  const test = 1
   return (
     <ReactSvg className={className} width={width} height={height} fill={fill} stroke={stroke} />
   )
